e147b95f76bf6ffc2b15895a218f44d9b0e82153
Achyut <actimsina@gmail.com>
2023-06-30 16:07:57 +0545

    testing upto user events

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Skills from './components/Skills';
+import Counter from './components/Counter';
 
 function App() {
+  const skills = [
+    { id: 1, name: "plumbing" },
+    { id: 2, name: "wiring" },
+    { id: 3, name: "painting" }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Skills skills={skills} /> */}
+      <Counter />
     </div>
   );
 }
